c59f2a0c2d0c206ef73fdde4e91baa9bea12d11c
HarshSaini3878 <142294106+HarshSaini3878@users.noreply.github.com>
2024-12-14 17:45:02 +0530

    initialised forntend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className="bg-red-500 text-xl text-white">
+        hi there
+      </div>
+       
+     
+    </>
+  )
+}
+
+export default App
